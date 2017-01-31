77489695dbf1d9a1ade5501b4c46ce2825c0df24
Eduard Dolynskyi <eduard.dolinskyi@binary-studio.com>
2017-01-31 23:59:15 +0200

    fix crud methods

--- app/entities/tasks/taskRoutes.js
+++ app/entities/tasks/taskRoutes.js
@@ -11,36 +11,36 @@ task.get('/', (req, res, next) => {
 	});
 });
 
-// task.post('/', (req, res, next) => {
-// 	taskService.addtask(req.body).then((task) => {
-// 		res.status(201).send(task);
-// 	}).catch((err) => {
-// 		res.status(400).end();
-// 	});
-// });
+task.post('/', (req, res, next) => {
+	taskService.addTask(req.body).then((task) => {
+		res.status(201).send(task);
+	}).catch((err) => {
+		res.status(400).end();
+	});
+});
 
-// task.get('/:id', (req, res, next) => {
-// 	taskService.gettaskById(req.params.id).then((task) => {
-// 		res.send(task);
-// 	}).catch((err) => {
-// 		res.status(400).end();
-// 	});
-// });
+task.get('/:id', (req, res, next) => {
+	taskService.getTaskById(req.params.id).then((task) => {
+		res.send(task);
+	}).catch((err) => {
+		res.status(400).end();
+	});
+});
 
-// task.put('/:id', (req, res, next) => {
-// 	taskService.edittask(req.params.id, req.body).then(() => {
-// 		res.end();
-// 	}).catch((err) => {
-// 		res.status(400).end();
-// 	});
-// });
+task.put('/:id', (req, res, next) => {
+	taskService.editTask(req.params.id, req.body).then(() => {
+		res.end();
+	}).catch((err) => {
+		res.status(400).end();
+	});
+});
 
-// task.delete('/:id', (req, res, next) => {
-// 	taskService.deletetask(req.params.id).then(() => {
-// 		res.status(200).end();
-// 	}).catch((err) => {
-// 		res.status(400).end();
-// 	});
-// });
+task.delete('/:id', (req, res, next) => {
+	taskService.deleteTask(req.params.id).then(() => {
+		res.status(200).end();
+	}).catch((err) => {
+		res.status(400).end();
+	});
+});
 
 module.exports = task;
